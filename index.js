6aa0b10a10fd426af77603d71861172b2e19e094
vaishnavihole <vaishnavihole1@gmail.com>
2023-03-16 14:43:49 +0530

    sample api added

--- index.js
+++ index.js
@@ -5,6 +5,17 @@ app.use(express.json())
 
 const PORT = 5000;
 
+app.get('/users', (req, res)=>{
+    res.json({
+      success: true,
+      data: "Users Data",
+      message: "Users fetched successfully"
+    })
+  })
+  
+
 app.listen(PORT, ()=>{
     console.log(`Server started on Port ${PORT}`)
   })
+
+  
