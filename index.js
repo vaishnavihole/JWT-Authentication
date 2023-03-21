8681f9e175f6c29b4e853929d485cc6a866efe74
vaishnavihole <vaishnavihole1@gmail.com>
2023-03-21 13:17:28 +0530

    install and import jwt

--- index.js
+++ index.js
@@ -1,18 +1,24 @@
 import express from "express";
+import jwt from "jsonwebtoken";
 
 const app = express();
 app.use(express.json())
 
 const PORT = 5000;
 
-app.post('/login', async(req, res) => {
+app.post('/login', async (req, res) => {
+
+
     res.json({
         success: true,
+        data: {
+            email: "xyz@1gmail.com"
+        },
+        message: "Logged in  Successful"
     })
 })
 
-app.listen(PORT, ()=>{
+app.listen(PORT, () => {
     console.log(`Server started on Port ${PORT}`)
-  })
+})
 
-  
