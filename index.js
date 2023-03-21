cf5bf3436370722d9ebe84a071ce6940155630c2
vaishnavihole <vaishnavihole1@gmail.com>
2023-03-21 12:51:25 +0530

    basic setup added

--- index.js
+++ index.js
@@ -5,23 +5,10 @@ app.use(express.json())
 
 const PORT = 5000;
 
-const posts = [
-    {
-        username: 'kyle',
-        title: 'Post 1'
-    },
-    {
-        username: 'Jim',
-        title: 'Post 2'
-    },
-]
 
-app.get('/posts', (req, res) => {
-    res.json(posts)
-})
 
 app.listen(PORT, ()=>{
-    console.log(`Server started on Port ${PORT}`)
+    console.log(`Server started on Port ${PORT}`)
   })
 
   
